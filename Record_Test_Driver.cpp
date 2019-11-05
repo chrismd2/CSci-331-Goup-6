7bff673859e73ed903bed5ae37f70332e751f2b1
Tylyr1 <laty1003@stcloudstate.edu>
2019-11-05 17:13:42 +0000

    added test driver

---
+++ Record_Test_Driver.cpp
@@ -0,0 +1,17 @@
+//#include "Record.cpp"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main()
+{
+  //test default constructor
+	//Record testRecord;
+
+  //testRecord.display();
+	
+	 
+	return 0;
+}
