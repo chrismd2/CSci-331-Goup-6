e93ce6b0a890f84bf569bf5e4e3251d7a944bc14
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2019-11-04 21:11:11 -0600

    Update Record.h
    Additions include:
      -  Header comment
      -  Constructor with elements of the record
      -  Constructor with strings for data items
      -  A get field function to reduce function variety for getters

--- Record.h
+++ Record.h
@@ -1,4 +1,16 @@
-//file Record.h
+/**---------------------------------------------------------------------------
+// @Record.h
+// Class Record (Contains information about individual zipcodes)
+// @author Tyler Lahr and Ryan Sweeney  (Additional comments by Mark Christenson)
+//---------------------------------------------------------------------------
+// Record class:  Used by Sequence Set Class
+//   includes additional features:
+//   -- Display the whole record it represents
+//   -- Display a field with in the record
+//   -- Return a field as a string
+//   -- Return the latitude
+//   -- Return the longitude
+//---------------------------------------------------------------------------**/
 
 #include <iostream>
 #include <string>
@@ -9,12 +21,17 @@ class Record
     // Access specifier 
     public: 
     Record();
+    Record(string _zip_code, string _place_name, string _state, string _county, Grid _gridPoint);
+    Record(string _zip_code, string _place_name, string _state, string _county, string latitude, string longitude);
     void display();
-    void display(string field);
+    void display(string field); //This might benefit from calling get_field
+    string get_field(string field); //This should have a switch statement
+    /**/
     string get_zip_code();
     string get_place_name();
     string get_state();
     string get_county();
+    /**/
     float get_latitude();
     float get_longitude();
 
