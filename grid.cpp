e9151e5cc5975e7fc1ec345011f7770d307efe24
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2019-11-04 21:38:43 -0600

    Update grid.cpp
    Provided comments for Grid

--- grid.cpp
+++ grid.cpp
@@ -1,3 +1,15 @@
+/**---------------------------------------------------------------------------
+// @Grid.cpp
+// Class Grid (manages a zipcode's grid information)
+// @author Seth Pomahatc and Sushan Tiwari  (Additional comments by Mark Christenson)
+//---------------------------------------------------------------------------
+// Grid class:  Used by Record Class
+//   includes additional features:
+//   -- Return the latitude
+//   -- Return the longitude
+//   -- Return distance to a provided grid from this grid
+//---------------------------------------------------------------------------**/
+
 #include <iostream>
 #include <math.h>
 const bool DEBUG = true;
@@ -30,25 +42,39 @@ Grid::Grid(float _latitude, float _longitude){
 	longitude = _longitude;
 }
 
-void Grid::setLatitude(float _latitude){
+void Grid::setLatitude(float _latitude){/**<setLatitude
+// Set Latitude for this grid object
+//   @pre _latitude must follow rules regarding floats
+//   @post Sets latitude for grid object*/
 	latitude = _latitude;
 }
 
-void Grid::setLongitude(float _longitude)
-{
+void Grid::setLongitude(float _longitude){/**<setLatitude
+// Set Longitude for this grid object
+//   @pre _longitude must follow rules regarding floats
+//   @post Sets longitude for grid object*/
 longitude = _longitude;  
 }
 
 
-float Grid::getLatitude(){
+float Grid::getLatitude(){/**<getLatitude
+// Get Latitude for this grid object
+//   @pre none
+//   @post returns latitude for grid object as float*/
   return latitude;
 }
 
-float Grid::getLongitude(){
+float Grid::getLongitude(){/**<getLongitude
+// Get Longitude for this grid object
+//   @pre none
+//   @post returns longitude for grid object as float*/
 		return longitude;
 }
 
-float Grid::getDistance(Grid _grid){
+float Grid::getDistance(Grid _grid){/**<getDistance
+// Get Distance from this grid object to another grid object
+//   @pre grid object must be provided
+//   @post returns distance from this grid object to another grid object as float*/
   float distance = pow(latitude - _grid.getLatitude(),2) + pow(longitude - _grid.getLongitude(),2);
   distance = sqrt(distance);
   return distance;
