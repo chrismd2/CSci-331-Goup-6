88a75ec06e2f326df41fc6e10b777f2e9aa24107
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2019-11-06 20:48:20 -0600

    Update Record.cpp
    Added:
         - Display {latitude, longitude}
         - get_field implementation
         - set_field implementation
    
    removed 
         - get_* functions
         - set_* functions not including {longitude_latitude, grid_point}

--- Record.cpp
+++ Record.cpp
@@ -60,88 +60,67 @@ void Record::display(string field)
     cout << county << endl;
   else if(field=="G" || field=="GRID")
     cout << gridPoint.getLatitude() << " " << gridPoint.getLongitude() << endl;
+  else if(field == "LAT" || field == "LATITUDE")
+    cout << gridPoint.getLatitude() << endl;
+  else if(field == "LONG" || field == "LONGITUDE")
+    cout << gridPoint.getLongitude() << endl;
   else
     cout << "Invalid field has been entered." << endl;
+}
 
+string Record::get_field(string field)
+{
+  string returnString;
+  for(int i = 0; field[i] != NULL; i++){
+      field[i] = toupper(field[i]);
+  }
+  //The following if else-if statements replace the case statement variation
+  if(field=="Z" || field=="ZIP")
+    returnString = zip_code;
+  else if(field=="CITY" || field=="P" || field=="PLACE_NAME")
+    returnString = place_name;
+  else if(field=="STATE")
+    returnString = state;
+  else if(field=="COUNTY")
+    returnString = county;
+  else if(field=="G" || field=="GRID")
+    returnString = gridPoint.getLatitude() + " " + gridPoint.getLongitude();
+  else if(field == "LAT" || field == "LATITUDE")
+    returnString = gridPoint.getLatitude();
+  else if(field == "LONG" || field == "LONGITUDE")
+    returnString = gridPoint.getLongitude();
+  else
+    returnString = "ERROR" + endl;
   
-  // switch(field)
-  //{
-  //   case Z:
-  //   case ZIP:
-  //     cout << zip_code << endl;
-  //     break;
-  //   case CITY:
-  //   case P:
-  //   case PLACE_NAME:
-  //     cout << place_name << endl;
-  //     break;
-  //   case STATE:
-  //     cout << state << endl;
-  //     break;
-  //   case COUNTY:
-  //     cout << county << endl;
-  //     break;
-  //   case G:
-  //   case GRID:
-  //     cout << gridPoint.getLatitude() << " " << gridPoint.getLongitude() << endl;
-  //     break;
-  //}
+  return returnString;
+}
+
+void Record::set_field(string field, string data)
+{
+  for(int i = 0; field[i] != NULL; i++){
+      field[i] = toupper(field[i]);
+  }
+  for(int i = 0; data[i] != NULL; i++){
+      data[i] = toupper(data[i]);
+  }
   
-}
-
-string Record::get_zip_code()
-{
-  return zip_code;
-}
-
-string Record::get_place_name()
-{
-  return place_name;
-}
-
-string Record::get_state()
-{
-  return state;
-}
-
-string Record::get_county()
-{
-  return county;
-}
-
-float Record::get_latitude()
-{
-  return gridPoint.getLatitude();
-}
-
-float Record::get_longitude()
-{
-  return gridPoint.getLongitude();
-}
-
-Grid Record::get_grid_point()
-{
-  return gridPoint;
-}
-
-void Record::set_zip_code(string _zip_code)
-{
-  zip_code = _zip_code;
-}
-
-void Record::set_place_name(string _place_name)
-{
-  place_name = _place_name;
-}
-
-void Record::set_state(string _state)
-{
-  state = _state;
-}
-
-void Record::set_county(string _county)
-{
-  county = _county;
+  //The following if else-if statements replace the case statement variation
+  if(field=="Z" || field=="ZIP")
+    zip_code = data;
+  else if(field=="CITY" || field=="P" || field=="PLACE_NAME")
+    place_name = data;
+  else if(field=="STATE")
+    state = data;
+  else if(field=="COUNTY")
+    county = data;
+  else if(field=="G" || field=="GRID")
+    cout << "grid setter needs to implemented";
+  else if(field == "LAT" || field == "LATITUDE")
+    gridPoint.setLatitude(data);
+  else if(field == "LONG" || field == "LONGITUDE")
+    gridPoint.setLongitude(data);
+  else
+    cout << "ERROR" << endl;
 }
 
 void Record::set_longitude_latitude(float longitude, float latitude)
