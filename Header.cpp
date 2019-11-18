1e270442cbc10ab799080f66d71e922b4107bbcf
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2019-11-18 16:08:44 -0600

    Merging the hard way

--- Header.cpp
+++ Header.cpp
@@ -4,10 +4,4 @@ extern const int ZIPLENGTH		= 6;						/**< Max length of the zip code in digits 
 extern const int RBNLENGTH        	= 8;						/**< Max length of the RBN code in digits */
 extern const int BLOCKLENGTH      	= RBNLENGTH + RECORDSPERBLOCK * ZIPLENGTH;	/**< Maximum length for the block	  */
 extern const double FILLPERCENT   	= 75;						/**< Max length of the RBN code in digits */
-extern const int BLOCKFILLCOUNT		= RECORDSPERBLOCK * (FILLPERCENT/100);		/**< Max length of the RBN code in digits */
-extern const int RECORDLENGTH		= 95;						/**< Length of a record in postal codes */
-extern const int POSTALHEADER		= 291;						/**< Length of a record in postal codes */
-//Record length: 95
-//Header length: 291
-//Record length: 95
-//Header length: 291
+extern const int BLOCKFILLCOUNT		  = RECORDSPERBLOCK * (FILLPERCENT/100);		/**< Max length of the RBN code in digits */
